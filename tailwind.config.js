860aaedb799c81353cfbd00ddbd7ae4ce299b832
Ketlyn Melo <ketlynmelo002@gmail.com>
2024-03-26 17:20:47 -0300

    quarto commit do app

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,8 +15,8 @@ export default {
 
     },
     fontFamily:{
-      'shouders': ' "Big Shourders Display" ',
-      'lexend': '"Lexend Deca'
+      'shoulders': ' "Big Shoulders Display" ',
+      'lexend': '"Lexend Deca"'
     }
   },
   plugins: [],
